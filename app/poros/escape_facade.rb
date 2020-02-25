b75423bf0f35dba0b983141ee113bcb2d71cebc2
Madelyn Romero <m.romero013@gmail.com>
2020-02-25 15:33:34 -0700

    Add logic for no type selected

--- app/poros/escape_facade.rb
+++ app/poros/escape_facade.rb
@@ -2,14 +2,18 @@ class EscapeFacade
   def initialize(params, location)
     @params = params
     @location = location
-  end 
+  end
 
   def climb_options
     results = EscapeService.new.get_climb_results(@params, @location)
     results.map do |result|
-      if result[:type] == @params[:type] || result[:type] == "Sport, TR"
+      if result[:type] == @params[:trad] || result[:type] == "Sport, TR"
         ClimbOptions.new(result)
-      end 
+      elsif result[:type] == @params[:sport] || result[:type] == "Sport, TR"
+        ClimbOptions.new(result)
+      elsif !@params.key?(:trad) && !@params.key?(:sport)
+        ClimbOptions.new(result)
+      end
     end.compact
   end
-end 
+end

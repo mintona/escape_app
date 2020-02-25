cb43f14f582724d25f2bfd1084502d0f5e082dbc
Madelyn Romero <m.romero013@gmail.com>
2020-02-25 16:56:33 -0700

    Add messy logic to compare incoming min diff and max diff

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,8 +1,13 @@
 class TripController < ApplicationController
   def new
+    if Integer(params[:min_diff].split('.').last) > Integer(params[:max_diff].split('.').last)
+      flash[:error] = "Maximum difficulty must be more than minimum difficulty."
+      redirect_to "/search/climbs/new"
+    else
     @location = session[:destination]
     render locals: {
       climb_results: EscapeFacade.new(params, @location).climb_options
     }
+    end
   end
 end

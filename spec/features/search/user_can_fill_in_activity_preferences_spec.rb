534829de73f8516c7d37d387e6e8e427d5ed5182
Madelyn Romero <m.romero013@gmail.com>
2020-02-25 16:55:01 -0700

    Add test for maximum difficulty must be greater than minimum difficulty

--- spec/features/search/user_can_fill_in_activity_preferences_spec.rb
+++ spec/features/search/user_can_fill_in_activity_preferences_spec.rb
@@ -100,7 +100,7 @@ describe "As a logged-in User, after choosing destination and activity" do
     expect(page).to have_css(".climbs")
   end
 
-  it "returns message if no climbs exist for those params" do
+  it "returns message if no climbs exist for those params and can return to climb params page" do
     WebMock.allow_net_connect!
     user = create(:user)
 
@@ -121,7 +121,33 @@ describe "As a logged-in User, after choosing destination and activity" do
     click_on "Find Climbs!"
 
     expect(current_path).to eq('/trip/new')
-    expect(page).to have_content("No climbs exist for these parameters.")
-    expect(page).not_to have_content("Climbing route options for your Trip!")
+    expect(page).to have_content("No climbs exist for those parameters.")
+    click_button "Try Again"
+
+    expect(current_path).to eq("/search/climbs/new")
+  end
+
+  it "returns flash message if min climb more than max climb" do
+    WebMock.allow_net_connect!
+    user = create(:user)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+    visit '/search'
+
+    fill_in "destination", with: "Boulder"
+
+    choose "Climbing"
+
+    click_button "Continue"
+
+    select "5.12", from: :min_diff
+    select "5.7", from: :max_diff
+    check('sport')
+
+    click_on "Find Climbs!"
+
+    expect(current_path).to eq('/search/climbs/new')
+    expect(page).to have_content("Maximum difficulty must be more than minimum difficulty.")
   end
 end

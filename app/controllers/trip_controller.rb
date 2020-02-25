aec9c1db8e30f4753e6248839b4a804430a5752f
Madelyn Romero <m.romero013@gmail.com>
2020-02-25 16:59:43 -0700

    Refactor min/max check to private helper method

--- app/controllers/trip_controller.rb
+++ app/controllers/trip_controller.rb
@@ -1,6 +1,6 @@
 class TripController < ApplicationController
   def new
-    if Integer(params[:min_diff].split('.').last) > Integer(params[:max_diff].split('.').last)
+    if min_greater_than_max?
       flash[:error] = "Maximum difficulty must be more than minimum difficulty."
       redirect_to "/search/climbs/new"
     else
@@ -9,5 +9,11 @@ class TripController < ApplicationController
       climb_results: EscapeFacade.new(params, @location).climb_options
     }
     end
+  end
+
+  private
+
+  def min_greater_than_max?
+    Integer(params[:min_diff].split('.').last) > Integer(params[:max_diff].split('.').last)
   end
 end

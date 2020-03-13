c976e9c8be9c56e4225208b4b37cb876c1157539
Alison Vermeil <alison.minton@gmail.com>
2020-03-13 17:03:52 -0600

    Add a hikes facade

---
+++ app/poros/hikes_facade.rb
@@ -0,0 +1,25 @@
+class HikesFacade
+  def initialize(destination, search_params)
+    @destination = Destination.new(destination)
+    @search_params = search_params
+  end
+
+  def destination_name
+    @destination.name
+  end
+
+  def destination_address
+    @destination.address
+  end
+
+  def number_of_hikes
+    hike_results.count
+  end
+
+  def hike_results
+    all_hikes = EscapeService.new.get_hike_results(@search_params, @destination.lat, @destination.lng)
+    @hike_options = all_hikes.map do |hike_response|
+      HikeOption.new(hike_response)
+    end
+  end
+end
